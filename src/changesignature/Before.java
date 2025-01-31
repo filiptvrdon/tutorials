58eae06a8423d88c85167017a1e682e10d9b62e1
filip.tvrdon <tvrdon.filip@gmail.com>
2025-01-31 07:06:47 +0100

    change signature reorder arguments

---
+++ src/changesignature/Before.java
@@ -0,0 +1,14 @@
+package changesignature;
+
+public class Before {
+
+  public static void main(String[] args) {
+    printValues("arguments right!", "the order of ","Please make ");
+
+  }
+
+  public static void printValues(String p3, String p2, String p1) {
+    System.out.println(p1 + p2 + p3);
+
+  }
+}
---
+++ src/changesignature/ReorderArguments.java
@@ -0,0 +1,12 @@
+package changesignature;
+
+public class ReorderArguments {
+
+  public static void main(String[] args) {
+    printValues("arguments right!", "the order of ","Please make ");
+  }
+
+  public static void printValues(String p3, String p2, String p1) {
+    System.out.println(p1 + p2 + p3);
+  }
+}
