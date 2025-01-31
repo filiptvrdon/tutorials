e48efb7dbfaaaef741b293dfa20f1b8dcb005541
filip.tvrdon <tvrdon.filip@gmail.com>
2025-01-31 16:02:22 +0100

    keyboard shortcuts

---
+++ src/keymap/KeyBoardShortcuts.java
@@ -0,0 +1,56 @@
+package keymap;
+
+public class KeyBoardShortcuts {
+
+
+  // BASIC NAVIGATION //
+
+  // ALT + 1    project structure view
+  // ALT + 2    file structure
+  // ALT + 3    terminal
+  // ALT + 4    run window
+  // ALT + 5    debug window
+  // ALT + 6    issues window
+  // ALT + 0    git window
+
+  // CTRL + SHIFT + 1, 1    open current file in project tree
+  //                          handy to help orient in the codebase
+
+  // CTRL + SHIFT + F       find in files
+  //                          tip1: when pressed in project view, will search within the selected file structure
+  //                          tip2: enables editing search results directly
+
+  // 2x SHIFT               find anything anywhere
+  //                          very powerful and surprisingly fast search across all project files
+  //                          especially good for looking up based on keywords
+  //                          might be too broad for some use cases
+
+  // CTRL + E   recent files (searchable)
+  // CTRL + T   re-open most recent file (multiple steps back, just like in browser)
+
+
+
+  // EDITOR
+
+  // CTRL + F     find
+  // CTRL + H     replace
+
+  // shortcuts withing find/replace view
+    // ALT + W      find only whole words
+    // ALT + C      case sensitive search
+
+  // CTRL + D                 delete line (custom)
+  // CTRL + SHIFT + UP/DOWN   duplicate line on line above/below
+  // CTRL + ALT + UP/DOWN     move line up/down
+
+
+
+
+
+
+
+
+
+
+
+}
