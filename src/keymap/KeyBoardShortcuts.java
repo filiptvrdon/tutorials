91b3b0b338923e2af00c74e4a47d32d3484d6a2e
Filip Tvrdoň <tvrdon.filip@gmail.com>
2025-02-09 06:13:22 +0100

    wip

--- src/keymap/KeyBoardShortcuts.java
+++ src/keymap/KeyBoardShortcuts.java
@@ -1,86 +1,56 @@
 package keymap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KeyBoardShortcuts {
 
+    private List<String> shortcuts = new ArrayList<>();
 
-  // BASIC NAVIGATION //
+    public static void main(String[] args) {
+        KeyBoardShortcuts keyBoardShortcuts = new KeyBoardShortcuts();
+        keyBoardShortcuts.loadShortcuts();
+        keyBoardShortcuts.printShortcuts();
+    }
 
-  // ALT + 1    project structure view
-  // ALT + 2    file structure
-  // ALT + 3    terminal
-  // ALT + 4    run window
-  // ALT + 5    debug window
-  // ALT + 6    issues window
-  // ALT + 0    git window
+    private void loadShortcuts() {
+        // BASIC NAVIGATION
+        shortcuts.add("ALT + 1 - project structure view");
+        shortcuts.add("ALT + 2 - file structure");
+        shortcuts.add("ALT + 3 - terminal");
+        shortcuts.add("ALT + 4 - run window");
+        shortcuts.add("ALT + 5 - debug window");
+        shortcuts.add("ALT + 6 - issues window");
+        shortcuts.add("ALT + 0 - git window");
 
+        // FILE NAVIGATION
+        shortcuts.add("CTRL + SHIFT + 1, 1 - open current file in project tree");
+        shortcuts.add("CTRL + SHIFT + F - find in files (tip: search within selected file structure, edit search results)");
+        shortcuts.add("2x SHIFT - find anything anywhere");
+        shortcuts.add("CTRL + E - recent files (searchable)");
+        shortcuts.add("CTRL + T - re-open most recent file (multiple steps back, like a browser)");
 
+        // EDITOR
+        shortcuts.add("CTRL + F - find");
+        shortcuts.add("CTRL + H - replace");
 
+        // Find/Replace options
+        shortcuts.add("ALT + W - find only whole words");
+        shortcuts.add("ALT + C - case sensitive search");
 
+        // Editing lines
+        shortcuts.add("CTRL + D - delete line (custom)");
+        shortcuts.add("CTRL + SHIFT + UP/DOWN - duplicate line above/below");
+        shortcuts.add("CTRL + ALT + UP/DOWN - move line up/down");
+    }
 
+    private void printShortcuts() {
+        for (String shortcut : shortcuts) {
+            printShortcut(shortcut);
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // CTRL + SHIFT + 1, 1    open current file in project tree
-  //                          handy to help orient in the codebase
-
-  // CTRL + SHIFT + F       find in files
-  //                          tip1: when pressed in project view, will search within the selected file structure
-  //                          tip2: enables editing search results directly
-
-  // 2x SHIFT               find anything anywhere
-  //                          very powerful and surprisingly fast search across all project files
-  //                          especially good for looking up based on keywords
-  //                          might be too broad for some use cases
-
-  // CTRL + E   recent files (searchable)
-  // CTRL + T   re-open most recent file (multiple steps back, just like in browser)
-
-
-
-  // EDITOR
-
-  // CTRL + F     find
-  // CTRL + H     replace
-
-  // shortcuts withing find/replace view
-    // ALT + W      find only whole words
-    // ALT + C      case sensitive search
-
-  // CTRL + D                 delete line (custom)
-  // CTRL + SHIFT + UP/DOWN   duplicate line on line above/below
-  // CTRL + ALT + UP/DOWN     move line up/down
-
-
-
-
-
-
-
-
-
-
-
+    private void printShortcut(String shortcut) {
+        System.out.println(shortcut);
+    }
 }
